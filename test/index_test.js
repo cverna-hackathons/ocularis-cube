b532e5187d15727d972fa024b77afaec6d4f57f0
Peter Berezny <spontain@gmail.com>
2016-03-01 15:43:16 +0100

    Fixing for build to pass

--- test/index_test.js
+++ test/index_test.js
@@ -3,6 +3,6 @@ import { strictEqual } from 'assert';
 
 describe('create', () => {
   it('returns 0 when either argument is 0', () => {
-    strictEqual(typeof(Component()), 'object');
+    strictEqual(typeof(Component), 'function');
   });
 });
